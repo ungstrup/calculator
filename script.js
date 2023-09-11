30137830955015820be8ec46b924176eb5046b80
ungstrup <filipung@gmail.com>
2023-09-11 17:38:21 +0200

    Change calculator behaviour, add backspace
    The calculator will now only allow to enter a single operator at a time.

--- script.js
+++ script.js
@@ -3,24 +3,50 @@ const sum = document.querySelector('#equal-button');
 const reset = document.querySelector('#clr-button');
 const buttons = document.querySelectorAll('.calc-button');
 
+let total = "";
+let operatorClicked = 0;
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (display.textContent === "Don't divide by zero" || display.textContent === "Error" || display.textContent === "0"){
             display.textContent = "";
+            total = "";
         };
-        if (button.value == "/" || button.value == "*" || button.value == "-" || button.value == "+") {
-            display.textContent += " " + button.value + " ";
-        } else display.textContent += button.value;
+        if (operatorClicked === 1){
+            display.textContent = "";
+            operatorClicked = 0;
+        };
+        if (button.value == "bck" && (total.charAt(total.length-1) != (" " || ""))){
+            total = total.slice(0, -1);
+            display.textContent = display.textContent.slice(0, -1);
+        } else if (button.value == "." && display.textContent.includes(".")){
+            return;
+        } else if ((button.value == "/" || button.value == "*" || button.value == "-" || button.value == "+") && (total.includes("/") || total.includes("*") || total.includes("-") || total.includes("+"))) {
+            total = calculator.operate(total);
+            display.textContent = total;
+            total += " " + button.value + " ";
+            operatorClicked = 1;
+        } else if (button.value == "/" || button.value == "*" || button.value == "-" || button.value == "+") {
+            total += " " + button.value + " ";
+            display.textContent = "";
+            operatorClicked = 1;
+        } else {
+            total += button.value;
+            display.textContent += button.value;
+        };
     });
 });
 
 sum.addEventListener('click', () => {
-    let calc = calculator.operate(display.textContent)
-    display.textContent = calc;
+    total = calculator.operate(total);
+    display.textContent = total;
+    operatorClicked = 0;
 });
 
 reset.addEventListener('click', () => {
-    display.textContent = "";
+    display.textContent = "0";
+    total = "";
+    operatorClicked = 0;
 });
 
 let calculator = {
@@ -35,18 +61,14 @@ let calculator = {
     },
     operate: function (str) {
         let split = str.split(" ");
-        let length = split.length;
+        if (split.length === 1) return str;
         let num = str;
-        for (let i = 1; i < length;){
-            let a = +split[0];
-            let operator = split[1];
-            let b = +split[2];
-            if (operator === "/" && b === 0) return "Don't divide by zero";
-            if (!this.operators[operator] || isNaN(a) || isNaN(b)) return "Error";
-            num = this.roundToFour(this.operators[operator](a, b));
-            split.splice(0, 3, num);
-            length = split.length;
-        };
-        return num;
+        let a = +split[0];
+        let operator = split[1];
+        let b = +split[2];
+        if (operator === "/" && b === 0) return "Don't divide by zero";
+        if (!this.operators[operator] || isNaN(a) || isNaN(b)) return "Error";
+        num = this.roundToFour(this.operators[operator](a, b));  
+        return num.toString();
     },
 };
